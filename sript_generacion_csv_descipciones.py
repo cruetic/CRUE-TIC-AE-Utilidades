79ba6ef8fe6b28a9dff86bed69103979513ae9b0
alu0100885453 <alu0100885453@ull.edu.es>
2019-03-20 10:25:08 +0000

    mejorando generador csv

--- sript_generacion_csv_descipciones.py
+++ sript_generacion_csv_descipciones.py
@@ -13,18 +13,24 @@ def cuales_son(archivos):
 		doc = minidom.parse(archivos)
 		itemlist = doc.getElementsByTagName("properties")
 		codigo = nombre = descripcion = tipo = ""
+		
 		for n in itemlist:
-		    
-		    key_n = n.attributes["key"].value
+		    sin_key = 0
+		    try:
+		    	key_n = n.attributes["key"].value
+		    except:
+		    	key_n = ""
+		    	sin_key = 1
 
 		    try:
 		    	value_n = n.attributes["value"].value
 		    except:
 		    	value_n = ""
 
+
 		    
-		    #print("\n\nkey_n   =" + key_n)
-		    #print("\nvalue_n =" + value_n)
+		    # print("\n\nkey_n   =" + key_n)
+		    # print("\nvalue_n =" + value_n)
 		    
 
 		    if key_n == "Código CRUE":
@@ -36,8 +42,23 @@ def cuales_son(archivos):
 		    if key_n == "Tipo":
 		    	tipo = value_n
 
+		    if sin_key == 1:
+		    	print("El Objeto " + codigo + " " + nombre)
+		    	if descripcion != "":
+		    		print(" con Descripción: \"" + descripcion + "\"")
+		    	print(" cuenta con una propiedad sin nombre definido.\n")
+
 		if(tipo == "Servicio"):
 			write.write(codigo + ";" + nombre + ";" + descripcion + "\n")
+			if(codigo == ""):
+				print("El Objeto " + nombre + " no tiene código definido")
+			if(nombre == ""):
+				print("El Objeto " + codigo + " no tiene nombre definido")
+			if(descripcion == ""):
+				print("El Objeto " + codigo + ", " + nombre + ", no tiene Descripción definida")
+		else:
+			if(codigo != "" or nombre != "" or descripcion != ""):
+				print("El Objeto " + codigo + " " + nombre + " cumple con las características de Servicio, pero no tiene atributo Tipo.\n")
 
 	read.close()
 	
@@ -57,15 +78,14 @@ def devolverArchivos(carpeta):
 
 
 
-write = open ("./generado.csv", "w")
+write = open ("./Servicios_CRUETIC.csv", "w")
 
 write.write("CATÁLOGO DE SERVICIOS TIC DE CRUE V3.0;;;;;;;\n")
 write.write("Cód. CRUE;Nombre del Servicio;Descripción del Servicio\n")
 
 
 
-if write:
-	print("Archivo .csv generado.\n")
+
 
 
 
@@ -74,4 +94,8 @@ devolverArchivos("C:/Users/Hp/AppData/Roaming/Archi4/model-repository/crue-tic-a
 
 #Ruta Pablo
 #devolverArchivos("C:/Users/Usuario/AppData/Roaming/Archi4/model-repository/archimate/model", objeto, key, value)
+
+if write:
+	print("\n---------------------------Archivo Servicios_CRUETIC.csv generado.---------------------------\n")
+
 write.close()
